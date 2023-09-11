25c96a34a030ed64fa3540420531f97415aedae4
André Silva <andreemanuel.1993@gmail.com>
2023-09-11 21:26:47 +0100

    Optimizes the Prague-Budapest images

---
+++ generate-travel-images.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const sharp = require('sharp');
+
+const originalImagesFolder = 'travel-images';
+const outputImagesFolder = 'public/images/travels';
+
+const travelNames = fs
+  .readdirSync(originalImagesFolder)
+  .filter((travelName) => !travelName.startsWith('.'));
+
+travelNames.forEach((travelName) => {
+  fs.readdirSync(`${originalImagesFolder}/${travelName}`)
+    .filter((imageName) => !imageName.startsWith('.'))
+    .forEach(async (imageName) => {
+      const fullPath = `${originalImagesFolder}/${travelName}/${imageName}`;
+      const outputFullPath = `${outputImagesFolder}/${travelName}/${imageName.split('.')[0]}.webp`;
+
+      await sharp(fullPath).resize(1000).toFile(outputFullPath);
+    });
+});
